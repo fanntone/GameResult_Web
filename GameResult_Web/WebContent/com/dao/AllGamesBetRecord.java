990fa938af249e80c86979db88c999fc19ed2ad4
fann.wu <fann.wu@gamefortunetech.com>
2017-02-20 15:30:31 +0800

    Fix AllGamesBetRecord.jsp query command.

--- GameResult_Web/WebContent/com/dao/AllGamesBetRecord.java
+++ GameResult_Web/WebContent/com/dao/AllGamesBetRecord.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -41,55 +42,32 @@ public class AllGamesBetRecord {
 	public List<Map<String, String>> getAllRecords(String date, String orderby, String asc) {
 		openConn(); 
 	    List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-    	String sql, sql_2, sql_3;
+    	String sql;
     	String orderby_str;
     	if(asc.equalsIgnoreCase("1"))
     		orderby_str = " order by " + orderby + " ASC;"; 
     	else
     		orderby_str = " order by " + orderby + " DESC;";
-    	sql = " select gameID, " 
-    		+ " count(distinct userID) as Players, "
-    		+ " count(betting) as Rounds, "
-    		+ " sum(betting) as Bet, "
-    		+ " sum(results) as Win, "
-    		+ " sum(CONVERT(betting, SIGNED) - CONVERT(results, SIGNED)) as Profit, "
-    		+ " sum(results)/sum(betting)*100 as PayRate "
-    		+ " from resultsRecords where Date(resultsDate) = "
-    		+ CommonString.TIMEDATE_QUATO + date + CommonString.TIMEDATE_QUATO;
-
-    	sql_2 = " select distinct gameID "
-        	  + " from resultsRecords where Date(resultsDate) = "
-        	  + CommonString.TIMEDATE_QUATO + date + CommonString.TIMEDATE_QUATO
-    	      + CommonString.SQLQUERYEND;
+    	sql = " select * " 
+    		+ " from betRecordsByDay where Date(times) = "
+    		+ CommonString.TIMEDATE_QUATO + date + CommonString.TIMEDATE_QUATO
+    		+ orderby_str
+    		+ CommonString.SQLQUERYEND;
 	    try {
-	    	psmt = conn.prepareStatement(sql_2);  
-	    	rs = psmt.executeQuery();
-	    	String[] gameID_array = new String[50];
-	    	int i = 0;
-	    	while(rs.next()) {
-	    		gameID_array[i] = rs.getString(CommonString.PARAMETER_GAMEID);
-	    		i++;	
-	    	}
-	    	for(int j = 0; j < i; j++) {
-	        	sql_3 = sql + " and gameID = " + gameID_array[j] + orderby_str;
-		    	psmt = conn.prepareStatement(sql_3);  
-		    	rs = psmt.executeQuery();  
+		    	psmt = conn.prepareStatement(sql);  
+		    	rs = psmt.executeQuery();
 		    	while(rs.next()) {
-			    	Map<String, String> map = new HashMap<String, String>();  
+		    		Map<String, String> map = new HashMap<String, String>();  
+		    		map.put(CommonString.TIMES, rs.getString(CommonString.TIMES));
 		    		map.put(CommonString.PARAMETER_GAMEID, rs.getString(CommonString.PARAMETER_GAMEID));
-		    		map.put(CommonString.PLAYERS, rs.getString(CommonString.PLAYERS));
-		    		map.put(CommonString.ROUNDS, rs.getString(CommonString.ROUNDS));
-		    		map.put(CommonString.BET, rs.getString(CommonString.BET));
-		    		map.put(CommonString.WIN, rs.getString(CommonString.WIN));
-		    		map.put(CommonString.PROFIT, rs.getString(CommonString.PROFIT));
-		    		String sum_bet = rs.getString(CommonString.BET);
-		    		if(sum_bet == null ||  sum_bet.equalsIgnoreCase("0") || sum_bet.equalsIgnoreCase(""))
-		    			map.put(CommonString.PAYRATE, "0");
-		    		else
-		    			map.put(CommonString.PAYRATE, rs.getString(CommonString.PAYRATE));
-		    		list.add(map);	
+		    		map.put(CommonString.PLAYERS, FormatDecimal(rs.getString(CommonString.PLAYERS)));
+		    		map.put(CommonString.ROUNDS, FormatDecimal(rs.getString(CommonString.ROUNDS)));
+		    		map.put(CommonString.BET, FormatDecimal(rs.getString(CommonString.BET)));
+		    		map.put(CommonString.WIN, FormatDecimal(rs.getString(CommonString.WIN)));
+		    		map.put(CommonString.PROFIT, FormatDecimal(rs.getString(CommonString.PROFIT)));
+		    		map.put(CommonString.PAYRATE, rs.getString(CommonString.PAYRATE));
+		    		list.add(map);
 		    	}
-	    	}
 
         } catch (SQLException e) {  
             e.printStackTrace();  
@@ -97,4 +75,10 @@ public class AllGamesBetRecord {
 	    closeConn();
 		return list;
 	}
+	
+    public String FormatDecimal(String x) {
+    	DecimalFormat df = new DecimalFormat("#,###");
+    	String s = df.format(Double.parseDouble(x));
+    	return s;
+    }
 }
